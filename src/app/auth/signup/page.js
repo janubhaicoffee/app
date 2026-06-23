500ebc1db7e0eea954fa7e267447b0514b0330b8
nothingness <janubhaicoffee@gmail.com>
2026-06-24 03:29:24 +0530

    u

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -24,6 +24,7 @@ export default function SignupPage() {
       email,
       password,
       options: {
+        emailRedirectTo: `${window.location.origin}/account`,
         data: {
           full_name: name,
         }
